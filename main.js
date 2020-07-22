b6aae02fe9c7716ddf31ea64697f03529d3fa2c1
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-07-22 16:44:40 -0500

    trying to figure out what is wrong with my display choices function

--- main.js
+++ main.js
@@ -18,24 +18,71 @@ class Characters {
 let designer = new Characters('Christina', 'designer')
 let headIntern = new Characters('Michaela', 'head intern')
 
-//--CLICK EVENTS--
-  for (let button of buttons) {
-    button.addEventListener('click', makeSelection)
+//--ALL CHOICES---
+const possibleChoices = [
+  {
+    id: 1,
+    text: `${designer.introduction} Welcome to the studio`,
+    options: [
+      {
+        text: 'I am ready!',
+        setState: { getStarted: true },
+        nextText: 2
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'Here are some things I\'d like for you to get done today. Which would you like to do first?',
+    options: [
+      {
+        text: 'Pick up dry cleaning.',
+        setState: {dryCleaningFirst: true},
+        nextText:3
+      },
+      {
+        text: 'Find swatches for a nude, sequin fabric.',
+        setState: {swatchesFirst: true},
+        nextText:4
+      }
+    ]
   }
 
+]
+
+//--CLICK EVENTS--
+  // for (let button of buttons) {
+  //   button.addEventListener('click', makeSelection)
+  // }
+
 //--FUNCTIONS--
-let playersDecisions = []
+const state = {}
+//created a game state as an empty object
 
-const mainGamePlay = () => {
+// const mainGamePlay = () => {
+//   //set the state as an empty object at first
+//   state = {}
+//   displayChoices(1)
+//   //run the function displayChoiceText for the first object in the choiceTexts array.
+//
+// }
 
+//-- original way that I wrote the displayChoices function --
+// const displayChoices = (possibleChoicesIndex) => {
+//   const choiceText = possibleChoices.find(choicesText => possibleChoices.id === possibleChoicesIndex)
+//   mainText.innerText = choiceText.text
+//   //text is not showing up. Something wrong with the syntax?
+// }
+
+//-- testing out why my function is not working... --
+function displayChoices(possibleChoicesIndex) {
+  const choiceText = possibleChoices.find(choiceText => possibleChoices.id === possibleChoicesIndex)
+
+  console.log(choiceText);
 }
-
-const displayDecisionText = (decisionIndex) => {
-
-}
-
-const makeSelection = (option) => {
-
-}
-
-mainGamePlay()
+displayChoices(1)
+// const makeSelection = (option) => {
+//
+// }
+//
+// mainGamePlay()
